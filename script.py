b22100dbb670b7d33c60cec10ea411f58b1e5489
fr-AlphaP <nicecapybara@proton.me>
2025-06-04 10:10:53 +0200

    patch 10h10é

--- script.py
+++ script.py
@@ -2,12 +2,26 @@
 import requests, json
 from rich import print_json
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=45.626804&longitude=1.036274&timezone=auto&hourly=wind_speed_10m&forecast_days=1&past_days=1"
+# L'URL reste la même car elle demande déjà toutes les données
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation,precipitation_probability,wind_speed_10m&past_days=14"
 data = requests.get(url).json()
 
 output_data = []
+data_hourly = data['hourly']
 
-for t, ws in zip(data['hourly']['time'], data['hourly']['wind_speed_10m']):
-    point = {"timestamp": t, "wind_speed_10m": ws}
+for a, b, c, d in zip(
+    data_hourly['time'],
+    data_hourly['temperature_2m'],
+    data_hourly['wind_speed_10m'],
+    data_hourly['precipitation_probability'],
+):
+    point = {
+        "timestamp": a,
+        "temperature_2m": b,
+        "wind_speed_10m": c,
+        "precipitation_probability": d,
+    }
     output_data.append(point)
+
+# On affiche le JSON final
 print_json(json.dumps(output_data))
